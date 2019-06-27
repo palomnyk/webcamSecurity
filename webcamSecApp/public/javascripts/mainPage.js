d22f65ef90be2edde52ba003b2489a9e263cb3a5
Aaron_Y <amyerke@uncc.edu>
2019-06-27 15:04:54 -0400

    the beginings of something useful

---
+++ webcamSecApp/public/javascripts/mainPage.js
@@ -0,0 +1,12 @@
+// (function(){
+  const mouseJail = document.getElementById('mouseJail');
+
+  console.log(mouseJail);
+  
+
+  mouseJail.addEventListener('mouseleave', console.log('mouse out of jail'));
+
+
+
+
+// })()
