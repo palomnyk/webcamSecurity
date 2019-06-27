d8aa62eb15998b1671c3a7e0d2b73adb27c362aa
Aaron_Y <amyerke@uncc.edu>
2019-06-27 15:46:42 -0400

    Got the basics working

--- webcamSecApp/public/javascripts/mainPage.js
+++ webcamSecApp/public/javascripts/mainPage.js
@@ -1,12 +1,27 @@
-// (function(){
-  const mouseJail = document.getElementById('mouseJail');
+(function(){
+  const mouseJail = document.getElementById('mouseJail'),
+    video = document.getElementById('video'),
+    canvas = document.getElementById("canvas"),
+    vendorUrl = window.URL;
 
-  console.log(mouseJail);
-  
+  let context = canvas.getContext('2d');
 
-  mouseJail.addEventListener('mouseleave', console.log('mouse out of jail'));
+    navigator.getMedia =  navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
 
-
-
-
-// })()
+  navigator.getMedia({
+    video: true,
+    audio: false,
+  }, function(stream){
+    video.srcObject=stream;;
+    video.play()
+  }, function(error){
+    console.error(error); 
+  });
+  mouseJail.addEventListener('mouseleave', function(){
+    console.log('mouse out of jail');
+    context.drawImage(video, 0, 0, 400, 300)
+});
+})()
