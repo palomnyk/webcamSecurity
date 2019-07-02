fdbc57a855eb039bce45a2d551481e6bcf4bc94d
Aaron_Y <amyerke@uncc.edu>
2019-07-02 11:16:40 -0400

    Put pic in this repository

--- takePicAtStartup/takePic.py
+++ takePicAtStartup/takePic.py
@@ -1,7 +1,11 @@
 import cv2
 import datetime
+import os
 
-datetime.datetime.now()
+now = datetime.datetime.now()
+
+#windows doesn't permit ":" in the file name
+now = str(now).replace(':', '-')
 
 video_capture = cv2.VideoCapture(0)
 # Check success
@@ -10,8 +14,14 @@ if not video_capture.isOpened():
 # Read picture. ret === True on success
 ret, frame = video_capture.read()
 
-cv2.imwrite(f'securityPic{datetime.datetime.now()}.png', video_capture.read()[1])
+#for putting code in the same dir as this script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+pic_path = os.path.join(dir_path, f"securityPic{now}.png")
+
+print(pic_path)
+
+cv2.imwrite(pic_path, video_capture.read()[1])
 
 # Close device
 video_capture.release()
-
